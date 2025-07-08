e4d113b7ad7c39e5a3948f068630e51425dfad6e
YiFei Zhu <zhuyifei@google.com>
2025-07-08 23:36:58 +0000

    iommufd-lu: Persist iommu hardware pagetables for live update
    The caller is expected to mark each HWPT to be preserved with an ioctl
    call, with a token that will be used in restore. At preserve time, each
    HWPT's domain is then called with iommu_domain_preserve to preserve the
    iommu domain.
    
    The HWPTs containing dma mappings backed by unpreserved memory should
    not be preserved. During preservation check if the mappings contained in
    the HWPT being preserved are only file based and all the files are
    preserved.
    
    The memfd file preservation check is not enough when preserving iommufd.
    The memfd might have shrinked between the mapping and memfd
    preservation. This means that if it shrinked some pages that are right
    now pinned due to iommu mappings are not preserved with the memfd. Only
    allow iommufd preservation when all the iopt_pages are file backed and
    the memory file was seal sealed during mapping. This guarantees that all
    the pages that were backing memfd when it was mapped are preserved.
    
    Once HWPT is preserved the iopt associated with the HWPT is made
    immutable. Since the map and unmap ioctls operates directly on iopt,
    which contains an array of domains, while each hwpt contains only one
    domain. The logic then becomes that mapping and unmapping is prohibited
    if any of the domains in an iopt belongs to a preserved hwpt. However,
    tracing to the hwpt through the domain is a lot more tedious than
    tracing through the ioas, so if an hwpt is preserved, hwpt->ioas->iopt
    is made immutable.
    
    When undoing this (making the iopts mutable again), there's never
    a need to make some iopts mutable and some kept immutable, since
    the undo only happen on unpreserve and error path of preserve.
    Simply iterate all the ioas and clear the immutability flag on all
    their iopts.
    
    Signed-off-by: YiFei Zhu <zhuyifei@google.com>
    Signed-off-by: Samiullah Khawaja <skhawaja@google.com>

--- drivers/iommu/iommufd/pages.c
+++ drivers/iommu/iommufd/pages.c
@@ -55,6 +55,7 @@
 #include <linux/overflow.h>
 #include <linux/slab.h>
 #include <linux/sched/mm.h>
+#include <linux/memfd.h>
 #include <linux/vfio_pci_core.h>
 
 #include "double_span.h"
@@ -1420,6 +1421,7 @@ struct iopt_pages *iopt_alloc_file_pages(struct file *file,
 
 {
 	struct iopt_pages *pages;
+	int seals;
 
 	pages = iopt_alloc_pages(start_byte, length, writable);
 	if (IS_ERR(pages))
@@ -1427,6 +1429,12 @@ struct iopt_pages *iopt_alloc_file_pages(struct file *file,
 	pages->file = get_file(file);
 	pages->start = start - start_byte;
 	pages->type = IOPT_ADDRESS_FILE;
+
+	pages->seals = 0;
+	seals = memfd_get_seals(file);
+	if (seals > 0)
+		pages->seals = seals;
+
 	return pages;
 }
 
