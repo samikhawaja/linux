19194abd58b6360e51559d6bec286db3ad5fd1ae
Samiullah Khawaja <skhawaja@google.com>
2025-09-20 00:04:32 +0000

    iommufd/selftest: Add test to verify iommufd preservation
    Test iommufd preservation by setting up an iommufd and vfio cdev and
    preserve it across live update. Test takes VFIO cdev path of a device
    bound to vfio-pci driver and binds it to an iommufd being preserved. It
    also preserves the vfio cdev so the iommufd state associated with it is
    also preserved.
    
    The restore path is tested by restoring the preserved vfio cdev, iommufd
    and HWPT and hotswapping it with the a new HWPT. Test also tries to
    finish the session before restoring iommufd, after restoring iommufd and
    after restoring HWPT to verify that it fails as the hotswap has not
    happened.
    
    Note that the helper functions setup_cdev, open_iommufd, and
    setup_iommufd will be replaced with VFIO selftest library. Similarly the
    helper function defined to open and interface with Live Update
    Orchestrator device will be replaced with a common helper library.
    
    Signed-off-by: Samiullah Khawaja <skhawaja@google.com>
    Signed-off-by: YiFei Zhu <zhuyifei@google.com>

--- tools/testing/selftests/iommu/iommufd_liveupdate.c
+++ tools/testing/selftests/iommu/iommufd_liveupdate.c
@@ -124,6 +124,140 @@ static int create_sealed_memfd(size_t size)
 	return fd;
 }
 
+int luo_session_finish(int session_fd)
+{
+	struct liveupdate_session_finish arg = { .size = sizeof(arg) };
+
+	if (ioctl(session_fd, LIVEUPDATE_SESSION_FINISH, &arg) < 0)
+		return -errno;
+
+	return 0;
+}
+
+int restore_iommufd(int session, int iommufd, int cdev_fd, int hwpt_token)
+{
+	int ret;
+
+	struct vfio_device_bind_iommufd bind = {
+		.argsz = sizeof(bind),
+		.flags = 0,
+	};
+	struct iommu_ioas_alloc alloc_data  = {
+		.size = sizeof(alloc_data),
+		.flags = 0,
+	};
+	struct iommu_hwpt_alloc hwpt_alloc = {
+		.size = sizeof(hwpt_alloc),
+		.flags = 0,
+	};
+	struct iommu_hwpt_lu_restore restore = {
+		.size = sizeof(restore),
+		.hwpt_token = hwpt_token,
+		.hwpt_alloc_flags = 0,
+	};
+	struct vfio_device_attach_iommufd_pt attach_data = {
+		.argsz = sizeof(attach_data),
+		.flags = 0,
+	};
+
+	bind.iommufd = iommufd;
+	ret = ioctl(cdev_fd, VFIO_DEVICE_BIND_IOMMUFD, &bind);
+	ksft_assert(!ret);
+
+	ret = ioctl(iommufd, IOMMU_IOAS_ALLOC, &alloc_data);
+	ksft_assert(!ret);
+
+	ret = ioctl(iommufd, IOMMU_HWPT_LU_RESTORE, &restore);
+	ksft_assert(!ret);
+
+	/* Should fail */
+	ret = luo_session_finish(session);
+	ksft_assert(ret);
+
+	hwpt_alloc.pt_id = bind.out_devid;
+	hwpt_alloc.pt_id = alloc_data.out_ioas_id;
+	ret = ioctl(iommufd, IOMMU_HWPT_ALLOC, &hwpt_alloc);
+	ksft_assert(ret);
+
+	attach_data.pt_id = hwpt_alloc.pt_id;
+	ret = ioctl(cdev_fd, VFIO_DEVICE_ATTACH_IOMMUFD_PT, &attach_data);
+	ksft_assert(!ret);
+	attach_data.pt_id = alloc_data.out_ioas_id;
+	ret = ioctl(cdev_fd, VFIO_DEVICE_ATTACH_IOMMUFD_PT, &attach_data);
+	ksft_assert(!ret);
+
+	return ret;
+}
+
+int open_liveupdate_orchestrator(void)
+{
+	int luo;
+
+	luo = open("/dev/liveupdate", O_RDWR);
+	ksft_assert(luo > 0);
+
+	return luo;
+}
+
+int luo_create_session(int luo_fd, const char *name)
+{
+	struct liveupdate_ioctl_create_session arg = { .size = sizeof(arg) };
+	int ret;
+
+	snprintf((char *)arg.name, LIVEUPDATE_SESSION_NAME_LENGTH, "%.*s",
+		 LIVEUPDATE_SESSION_NAME_LENGTH - 1, name);
+	ret = ioctl(luo_fd, LIVEUPDATE_IOCTL_CREATE_SESSION, &arg);
+	ksft_assert(!ret);
+	ksft_assert(arg.fd > 0);
+
+	return arg.fd;
+}
+
+int luo_retrieve_session(int luo_fd, const char *name)
+{
+	struct liveupdate_ioctl_retrieve_session arg = { .size = sizeof(arg) };
+	int ret;
+
+	snprintf((char *)arg.name, LIVEUPDATE_SESSION_NAME_LENGTH, "%.*s",
+		 LIVEUPDATE_SESSION_NAME_LENGTH - 1, name);
+	ret = ioctl(luo_fd, LIVEUPDATE_IOCTL_RETRIEVE_SESSION, &arg);
+	ksft_assert(!ret || errno == ENOENT);
+
+	if (ret && errno == ENOENT)
+		return -errno;
+
+	return arg.fd;
+}
+
+int liveupdate_preserve_fd(int session_fd, int fd, int token)
+{
+	struct liveupdate_session_preserve_fd preserve;
+	int ret;
+
+	preserve.fd = fd;
+	preserve.token = token;
+	preserve.size = sizeof(preserve);
+
+	ret = ioctl(session_fd, LIVEUPDATE_SESSION_PRESERVE_FD, &preserve);
+	ksft_assert(!ret);
+
+	return ret;
+}
+
+int liveupdate_restore_fd(int session_fd, int token)
+{
+	struct liveupdate_session_retrieve_fd arg = { .size = sizeof(arg) };
+	int ret;
+
+	arg.token = token;
+
+	ret = ioctl(session_fd, LIVEUPDATE_SESSION_RETRIEVE_FD, &arg);
+	ksft_assert(!ret);
+	ksft_assert(arg.fd > 0);
+
+	return arg.fd;
+}
+
 int main(int argc, char *argv[])
 {
 	int iommufd, cdev_fd, memfd, luo, session, ret;
@@ -200,9 +334,18 @@ int main(int argc, char *argv[])
 		ksft_assert(ret);
 		ksft_assert(errno == EPERM);
 
+		iommufd = liveupdate_restore_fd(session, token);
+		cdev_fd = liveupdate_restore_fd(session, cdev_token);
+
 		/* Should fail */
 		ret = luo_session_finish(session);
 		ksft_assert(ret);
+
+		ret = restore_iommufd(session, iommufd, cdev_fd, hwpt_token);
+		ksft_assert(!ret);
+
+		ret = luo_session_finish(session);
+		ksft_assert(!ret);
 	}
 
 	return 0;
