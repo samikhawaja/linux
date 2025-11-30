c35a8024998202a55c9c3a36f001b65ce1274717
Samiullah Khawaja <skhawaja@google.com>
2025-11-30 03:57:29 +0000

    iommu: Implement device and IOMMU HW preservation
    Add IOMMU ops to preserve/unpreserve the device state handled by the
    IOMMU driver. This is IOMMU specific state of the device that needs to
    be preserved for liveupdate. These ops can be implemented by the IOMMU
    drivers that support preservation of devices that have their IOMMU
    domains preserved. During device preservation the state of the
    associated IOMMU is also preserved as dependency.
    
    Signed-off-by: Samiullah Khawaja <skhawaja@google.com>

--- include/linux/iommu-liveupdate.h
+++ include/linux/iommu-liveupdate.h
@@ -8,14 +8,42 @@
 #ifndef _LINUX_IOMMU_LIVEUPDATE_H
 #define _LINUX_IOMMU_LIVEUPDATE_H
 
+#include <linux/device.h>
 #include <linux/iommu.h>
 #include <linux/liveupdate.h>
 #include <linux/kho/abi/iommu.h>
 
 #ifdef CONFIG_IOMMU_LIVEUPDATE
+static inline void *dev_iommu_preserved_state(struct device *dev)
+{
+	struct iommu_device_ser *ser;
+
+	if (!dev->iommu)
+		return NULL;
+
+	ser = dev->iommu->device_ser;
+	if (ser && !(ser->hdr.flags & IOMMU_SER_FLAG_INCOMING))
+		return ser;
+
+	return NULL;
+}
+
 int iommu_preserve_domain(struct iommu_domain *domain, struct iommu_domain_ser **ser);
 void iommu_unpreserve_domain(struct iommu_domain *domain);
+int iommu_preserve_device(struct iommu_domain *domain,
+			  struct device *dev, u64 *preserved_state);
+void iommu_unpreserve_device(struct iommu_domain *domain, struct device *dev);
+
+static inline void *iommu_preserved_state(struct iommu_device *iommu)
+{
+	return iommu->outgoing_preserved_state;
+}
 #else
+static inline void *dev_iommu_preserved_state(struct device *dev)
+{
+	return NULL;
+}
+
 static inline int iommu_preserve_domain(struct iommu_domain *domain, struct iommu_domain_ser **ser)
 {
 	return -EOPNOTSUPP;
@@ -23,6 +51,21 @@ static inline int iommu_preserve_domain(struct iommu_domain *domain, struct iomm
 
 static inline void iommu_unpreserve_domain(struct iommu_domain *domain)
 {
+}
+
+static inline int iommu_preserve_device(struct iommu_domain *domain,
+					struct device *dev, u64 *preserved_state)
+{
+	return -EOPNOTSUPP;
+}
+
+static inline void iommu_unpreserve_device(struct iommu_domain *domain, struct device *dev)
+{
+}
+
+static inline void *iommu_preserved_state(struct iommu_device *iommu)
+{
+	return NULL;
 }
 #endif
 
