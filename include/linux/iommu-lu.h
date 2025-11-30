3d1847e1ddc16840e85c91324e834571fa24d404
Samiullah Khawaja <skhawaja@google.com>
2025-11-30 04:26:04 +0000

    iommu: Add APIs to preserve/unpreserve iommu domains
    These APIs will be used by iommufd LU handler to preserve and unpreserve
    a domain for live update.
    
    Signed-off-by: Samiullah Khawaja <skhawaja@google.com>

--- include/linux/iommu-lu.h
+++ include/linux/iommu-lu.h
@@ -11,6 +11,8 @@
 #include <linux/liveupdate.h>
 #include <linux/kho/abi/iommu.h>
 
+int iommu_domain_preserve(struct iommu_domain *domain, struct iommu_domain_ser **ser);
+int iommu_domain_unpreserve(struct iommu_domain *domain);
 int iommu_liveupdate_register_flb(struct liveupdate_file_handler *handler);
 int iommu_liveupdate_unregister_flb(struct liveupdate_file_handler *handler);
 
