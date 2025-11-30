0c41de9db7a37fed2c225af0f363c47b89218ba8
Samiullah Khawaja <skhawaja@google.com>
2025-11-30 04:37:10 +0000

    iommu: Add API to keep track of iommu domain attachments
    Keep track of iommu domain attachments by incrementing and decremeting a
    counter when a device is attached and detached to a domain.
    
    Signed-off-by: Samiullah Khawaja <skhawaja@google.com>

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -251,6 +251,7 @@ struct iommu_domain {
 		};
 	};
 
+	atomic_t attach_count;
 #ifdef CONFIG_IOMMU_LIVEUPDATE
 	struct iommu_domain_ser *preserved_state;
 #endif
@@ -961,6 +962,7 @@ static inline struct iommu_domain *iommu_paging_domain_alloc(struct device *dev)
 {
 	return iommu_paging_domain_alloc_flags(dev, 0);
 }
+extern bool iommu_domain_has_attachments(struct iommu_domain *domain);
 extern void iommu_domain_free(struct iommu_domain *domain);
 extern int iommu_attach_device(struct iommu_domain *domain,
 			       struct device *dev);
