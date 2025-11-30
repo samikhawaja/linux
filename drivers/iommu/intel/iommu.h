9d64d111f328164d940b4f53281c9e2ceb7a6eb4
Samiullah Khawaja <skhawaja@google.com>
2025-11-30 22:36:42 +0000

    iommu/vt-d: Restore IOMMU state and reclaimed domain ids
    During boot fetch the preserved state of IOMMU unit and if found then
    restore the state.
    
    - Reuse the root_table that was preserved in the previous kernel.
    - Reclaim the domain ids of the preserved domains for each preserved
      devices so these are not acquired by another domain.
    
    Signed-off-by: Samiullah Khawaja <skhawaja@google.com>

--- drivers/iommu/intel/iommu.h
+++ drivers/iommu/intel/iommu.h
@@ -1317,6 +1317,8 @@ int intel_iommu_preserve(struct iommu_device *iommu,
 void intel_iommu_unpreserve(struct iommu_device *iommu,
 			    struct iommu_hw_ser *iommu_ser);
 void clear_unpreserved_context_entries(struct intel_iommu *iommu);
+void intel_iommu_liveupdate_restore_root_table(struct intel_iommu *iommu,
+					       struct iommu_hw_ser *iommu_ser);
 #else
 static inline int intel_iommu_preserve_device(struct device *dev,
 					      struct iommu_device_ser *device_ser)
@@ -1341,6 +1343,11 @@ static inline void intel_iommu_unpreserve(struct iommu_device *iommu,
 }
 
 static inline void clear_unpreserved_context_entries(struct intel_iommu *iommu)
+{
+}
+
+static inline void intel_iommu_liveupdate_restore_root_table(struct intel_iommu *iommu,
+							     struct iommu_hw_ser *iommu_ser)
 {
 }
 #endif
