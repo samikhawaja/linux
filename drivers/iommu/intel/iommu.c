95b969d6e898e7628afab8ee0865b5edc7c986d7
Samiullah Khawaja <skhawaja@google.com>
2025-11-30 05:16:49 +0000

    iommu/vt-d: Implement device and iommu preserve/unpreserve ops
    Add implementation of the device and iommu presevation in a separate
    file. Also set the device and iommu preserve/unpreserve ops in the
    struct iommu_ops.
    
    During normal shutdown the iommu translation is disabled. Since the root
    table is preserved during live update, it needs to be cleaned up and the
    context entries of the unpreserved devices need to be cleared.
    
    Signed-off-by: Samiullah Khawaja <skhawaja@google.com>

--- drivers/iommu/intel/iommu.c
+++ drivers/iommu/intel/iommu.c
@@ -16,6 +16,7 @@
 #include <linux/crash_dump.h>
 #include <linux/dma-direct.h>
 #include <linux/dmi.h>
+#include <linux/iommu-lu.h>
 #include <linux/memory.h>
 #include <linux/pci.h>
 #include <linux/pci-ats.h>
@@ -52,6 +53,8 @@ static int rwbf_quirk;
 
 #define rwbf_required(iommu)	(rwbf_quirk || cap_rwbf((iommu)->cap))
 
+static bool __maybe_clean_unpreserved_context_entries(struct intel_iommu *iommu);
+
 /*
  * set to 1 to panic kernel if can't successfully enable VT-d
  * (used when kernel is launched w/ TXT)
@@ -59,8 +62,6 @@ static int rwbf_quirk;
 static int force_on = 0;
 static int intel_iommu_tboot_noforce;
 static int no_platform_optin;
-
-#define ROOT_ENTRY_NR (VTD_PAGE_SIZE/sizeof(struct root_entry))
 
 /*
  * Take a root_entry and return the Lower Context Table Pointer (LCTP)
@@ -2375,8 +2376,10 @@ void intel_iommu_shutdown(void)
 		/* Disable PMRs explicitly here. */
 		iommu_disable_protect_mem_regions(iommu);
 
-		/* Make sure the IOMMUs are switched off */
-		iommu_disable_translation(iommu);
+		if (!__maybe_clean_unpreserved_context_entries(iommu)) {
+			/* Make sure the IOMMUs are switched off */
+			iommu_disable_translation(iommu);
+		}
 	}
 }
 
@@ -2898,6 +2901,38 @@ static const struct iommu_dirty_ops intel_second_stage_dirty_ops = {
 	IOMMU_PT_DIRTY_OPS(vtdss),
 	.set_dirty_tracking = intel_iommu_set_dirty_tracking,
 };
+
+#ifdef CONFIG_IOMMU_LIVEUPDATE
+static bool __maybe_clean_unpreserved_context_entries(struct intel_iommu *iommu)
+{
+	struct device_domain_info *info;
+	struct pci_dev *pdev = NULL;
+
+	if (!iommu->iommu.outgoing_preserved_state)
+		return false;
+
+	for_each_pci_dev(pdev) {
+		info = dev_iommu_priv_get(&pdev->dev);
+		if (!info)
+			continue;
+
+		if (info->iommu != iommu)
+			continue;
+
+		if (dev_iommu_preserved_state(&pdev->dev))
+			continue;
+
+		domain_context_clear(info);
+	}
+
+	return true;
+}
+#else
+static bool __maybe_clean_unpreserved_context_entries(struct intel_iommu *iommu)
+{
+	return false;
+}
+#endif
 
 static struct iommu_domain *
 intel_iommu_domain_alloc_second_stage(struct device *dev,
@@ -3924,6 +3959,10 @@ const struct iommu_ops intel_iommu_ops = {
 	.is_attach_deferred	= intel_iommu_is_attach_deferred,
 	.def_domain_type	= device_def_domain_type,
 	.page_response		= intel_iommu_page_response,
+	.preserve_device	= intel_iommu_preserve_device,
+	.unpreserve_device	= intel_iommu_unpreserve_device,
+	.preserve		= intel_iommu_preserve,
+	.unpreserve		= intel_iommu_unpreserve,
 };
 
 static void quirk_iommu_igfx(struct pci_dev *dev)
