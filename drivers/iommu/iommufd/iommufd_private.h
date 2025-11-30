e19e5f9c2f6d56e37c1c0090b4ba6fde27266bab
Samiullah Khawaja <skhawaja@google.com>
2025-11-30 05:29:24 +0000

    iommufd: Add APIs to preserve/unpreserve a vfio cdev
    Add APIs that can be used to preserve and unpreserve a vfio cdev. Use
    the APIs exported by the IOMMU core to preserve/unpreserve device.
    
    The LUO token of the preserved iommufd is fetched and returned back to
    the caller as that can be used during restore to get the restored
    iommufd. Handle to the preserved state of the device is also returned to
    reassociate with the restored state after live update kexec.
    
    Signed-off-by: Samiullah Khawaja <skhawaja@google.com>

--- drivers/iommu/iommufd/iommufd_private.h
+++ drivers/iommu/iommufd/iommufd_private.h
@@ -485,6 +485,9 @@ struct iommufd_group {
 	struct xarray pasid_attach;
 	struct iommufd_sw_msi_maps required_sw_msi;
 	phys_addr_t sw_msi_start;
+#ifdef CONFIG_IOMMU_LIVEUPDATE
+	bool liveupdate_preserved;
+#endif
 };
 
 /*
