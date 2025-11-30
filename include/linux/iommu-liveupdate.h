0b6140a7c5fd02a0a73fc2d5895e616959ba2199
Samiullah Khawaja <skhawaja@google.com>
2025-11-30 03:57:29 +0000

    iommu: Implement IOMMU domain preservation
    Add IOMMU domain ops that can be implemented by the IOMMU drivers if
    they support IOMMU domain preservation across liveupdate. The new IOMMU
    domain preserve, unpreserve and restore APIs call these ops to perform
    respective live update operations.
    
    Reviewed-by: Pranjal Shrivastava <praan@google.com>
    Signed-off-by: Samiullah Khawaja <skhawaja@google.com>

--- include/linux/iommu-liveupdate.h
+++ include/linux/iommu-liveupdate.h
@@ -12,6 +12,20 @@
 #include <linux/liveupdate.h>
 #include <linux/kho/abi/iommu.h>
 
+#ifdef CONFIG_IOMMU_LIVEUPDATE
+int iommu_preserve_domain(struct iommu_domain *domain, struct iommu_domain_ser **ser);
+void iommu_unpreserve_domain(struct iommu_domain *domain);
+#else
+static inline int iommu_preserve_domain(struct iommu_domain *domain, struct iommu_domain_ser **ser)
+{
+	return -EOPNOTSUPP;
+}
+
+static inline void iommu_unpreserve_domain(struct iommu_domain *domain)
+{
+}
+#endif
+
 int iommu_liveupdate_register_flb(struct liveupdate_file_handler *handler);
 void iommu_liveupdate_unregister_flb(struct liveupdate_file_handler *handler);
 
