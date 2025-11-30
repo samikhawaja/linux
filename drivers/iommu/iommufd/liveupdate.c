8572e4f5fbbc5623f0dd37c1b54a8bbcacd72fdc
Samiullah Khawaja <skhawaja@google.com>
2025-11-30 04:41:18 +0000

    iommufd: Handle the iommufd can_finish properly
    IOMMUFD cannot finish until the restored domains have attachments. The
    devices attached to the restored domains should be detached or
    hotswapped to different iommu domains before the finish can happen.
    
    Signed-off-by: Samiullah Khawaja <skhawaja@google.com>

--- drivers/iommu/iommufd/liveupdate.c
+++ drivers/iommu/iommufd/liveupdate.c
@@ -272,16 +272,6 @@ err_folio_put:
 	return rc;
 }
 
-static bool iommufd_liveupdate_can_finish(struct liveupdate_file_op_args *args)
-{
-	if (!args->retrieved || !args->file) {
-		pr_warn("%s: fd not reclaimed\n", __func__);
-		return false;
-	}
-
-	return true;
-}
-
 int iommufd_hwpt_lu_restore(struct iommufd_ucmd *ucmd)
 {
 	struct iommu_hwpt_lu_restore *cmd = ucmd->cmd;
@@ -325,12 +315,57 @@ hwpt_found:
 	iommufd_object_finalize(ictx, &hwpt->common.obj);
 
 	hwpt_lu->reclaimed = true;
+	hwpt->lu_restored = true;
 	cmd->pt_id = hwpt->common.obj.id;
 	return 0;
 
 err_destroy:
 	iommufd_object_abort_and_destroy(ictx, &hwpt->common.obj);
 	return rc;
+}
+
+static bool iommufd_liveupdate_can_finish(struct liveupdate_file_op_args *args)
+{
+	struct iommufd_hwpt_paging *hwpt;
+	struct iommufd_hwpt_lu *hwpt_lu;
+	struct iommufd_lu *iommufd_lu;
+	struct iommufd_object *obj;
+	struct iommufd_ctx *ictx;
+	unsigned long index;
+	unsigned int i;
+
+	if (!args->retrieved || !args->file) {
+		pr_warn("%s: fd not reclaimed\n", __func__);
+		return false;
+	}
+
+	ictx = iommufd_ctx_from_file(args->file);
+	iommufd_lu = ictx->lu;
+
+	for (i = 0; i < iommufd_lu->nr_hwpts; i++) {
+		hwpt_lu = &iommufd_lu->hwpts[i];
+
+		if (!hwpt_lu->reclaimed)
+			return false;
+	}
+
+	xa_lock(&ictx->objects);
+	xa_for_each(&ictx->objects, index, obj) {
+		if (obj->type != IOMMUFD_OBJ_HWPT_PAGING)
+			continue;
+
+		hwpt = container_of(obj, struct iommufd_hwpt_paging, common.obj);
+		if (!hwpt->lu_restored)
+			continue;
+
+		if (!hwpt->common.domain || iommu_domain_has_attachments(hwpt->common.domain)) {
+			xa_unlock(&ictx->objects);
+			return false;
+		}
+	}
+	xa_unlock(&ictx->objects);
+
+	return true;
 }
 
 static void iommufd_liveupdate_finish(struct liveupdate_file_op_args *args)
