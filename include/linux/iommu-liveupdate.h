76c569f64ef8ecf63e8d0f8030fae113a5b4f11d
Samiullah Khawaja <skhawaja@google.com>
2025-11-30 03:57:29 +0000

    iommu: Implement device and IOMMU HW peservation
    Add IOMMU ops to preserve/unpreserve a device. These can be implemented
    by the IOMMU drivers that support preservation of devices that have
    their IOMMU domains preserved. During device preservation the state of
    the associated IOMMU is also preserved as dependency.
    
    Signed-off-by: Samiullah Khawaja <skhawaja@google.com>

--- include/linux/iommu-liveupdate.h
+++ include/linux/iommu-liveupdate.h
@@ -8,20 +8,53 @@
 #ifndef _LINUX_IOMMU_LIVEUPDATE_H
 #define _LINUX_IOMMU_LIVEUPDATE_H
 
+#include <linux/device.h>
 #include <linux/iommu.h>
 #include <linux/liveupdate.h>
 #include <linux/kho/abi/iommu.h>
 
 #ifdef CONFIG_IOMMU_LIVEUPDATE
+static inline void *dev_iommu_preserved_state(struct device *dev)
+{
+	struct iommu_device_ser *ser;
+
+	if (!dev->iommu)
+		return NULL;
+
+	ser = dev->iommu->device_ser;
+	if (ser && !ser->hdr.incoming)
+		return ser;
+
+	return NULL;
+}
+
 int iommu_domain_preserve(struct iommu_domain *domain, struct iommu_domain_ser **ser);
 void iommu_domain_unpreserve(struct iommu_domain *domain);
+int iommu_preserve_device(struct iommu_domain *domain,
+			  struct device *dev, u64 *preserved_state);
+void iommu_unpreserve_device(struct iommu_domain *domain, struct device *dev);
 #else
+static inline void *dev_iommu_preserved_state(struct device *dev)
+{
+	return NULL;
+}
+
 static inline int iommu_domain_preserve(struct iommu_domain *domain, struct iommu_domain_ser **ser)
 {
 	return -EOPNOTSUPP;
 }
 
 static inline void iommu_domain_unpreserve(struct iommu_domain *domain)
+{
+}
+
+static inline int iommu_preserve_device(struct iommu_domain *domain,
+					struct device *dev, u64 *preserved_state)
+{
+	return -EOPNOTSUPP;
+}
+
+static inline void iommu_unpreserve_device(struct iommu_domain *domain, struct device *dev)
 {
 }
 #endif
