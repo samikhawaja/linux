1bc6fac648b363e7078a3292742f00f016470e9e
Samiullah Khawaja <skhawaja@google.com>
2025-12-17 00:38:31 +0000

    iommu/vt-d: preserve PASID table of preserved device
    In scalable mode the PASID table is used to fetch the io page tables.
    Preserve and restore the PASID table of the preserved devices.
    
    Signed-off-by: Samiullah Khawaja <skhawaja@google.com>

--- drivers/iommu/intel/iommu.c
+++ drivers/iommu/intel/iommu.c
@@ -3999,6 +3999,7 @@ const struct iommu_ops intel_iommu_ops = {
 	.page_response		= intel_iommu_page_response,
 #ifdef CONFIG_IOMMU_LIVEUPDATE
 	.preserve_device	= intel_iommu_preserve_device,
+	.unpreserve_device	= intel_iommu_unpreserve_device,
 	.preserve		= intel_iommu_preserve,
 	.unpreserve		= intel_iommu_unpreserve,
 #endif
