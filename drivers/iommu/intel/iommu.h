0b15731701ae26d4ae80130c43ce1321bccbe528
Samiullah Khawaja <skhawaja@google.com>
2025-12-17 00:38:31 +0000

    iommu/vt-d: preserve PASID table of preserved device
    In scalable mode the PASID table is used to fetch the io page tables.
    Preserve and restore the PASID table of the preserved devices.
    
    Signed-off-by: Samiullah Khawaja <skhawaja@google.com>

--- drivers/iommu/intel/iommu.h
+++ drivers/iommu/intel/iommu.h
@@ -1289,6 +1289,8 @@ static inline int iopf_for_domain_replace(struct iommu_domain *new,
 #ifdef CONFIG_IOMMU_LIVEUPDATE
 int intel_iommu_preserve_device(struct device *dev,
 				struct iommu_device_ser *device_ser);
+void intel_iommu_unpreserve_device(struct device *dev,
+				   struct iommu_device_ser *device_ser);
 int intel_iommu_preserve(struct iommu_device *iommu,
 			 struct iommu_hw_ser *iommu_ser);
 void intel_iommu_unpreserve(struct iommu_device *iommu,
@@ -1303,6 +1305,11 @@ static inline int intel_iommu_preserve_device(struct device *dev,
 					      struct iommu_device_ser *device_ser)
 {
 	return -EOPNOTSUPP;
+}
+
+static inline void intel_iommu_unpreserve_device(struct device *dev,
+						 struct iommu_device_ser *device_ser)
+{
 }
 
 static inline int intel_iommu_preserve(struct iommu_device *iommu,
@@ -1320,6 +1327,7 @@ static inline void intel_iommu_liveupdate_restore_root_table(struct intel_iommu 
 							     struct iommu_hw_ser *iommu_ser)
 {
 }
+
 static inline int intel_iommu_domain_reattach_iommu(struct dmar_domain *domain,
 						    struct intel_iommu *iommu,
 						    struct iommu_device_ser *device_ser)
