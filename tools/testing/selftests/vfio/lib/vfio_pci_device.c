4e502b2b94b8f1f367ca3a9f9c2e6133d4214e24
Samiullah Khawaja <skhawaja@google.com>
2025-12-19 21:15:38 +0000

    vfio: selftests: Add support of creating iommus from iommufd
    Add API to init a struct iommu using an already opened iommufd instance
    and attach devices to it.
    
    Signed-off-by: Samiullah Khawaja <skhawaja@google.com>

--- tools/testing/selftests/vfio/lib/vfio_pci_device.c
+++ tools/testing/selftests/vfio/lib/vfio_pci_device.c
@@ -302,7 +302,7 @@ const char *vfio_pci_get_cdev_path(const char *bdf)
 	return cdev_path;
 }
 
-static void vfio_device_bind_iommufd(int device_fd, int iommufd)
+static int vfio_device_bind_iommufd(int device_fd, int iommufd)
 {
 	struct vfio_device_bind_iommufd args = {
 		.argsz = sizeof(args),
@@ -310,6 +310,7 @@ static void vfio_device_bind_iommufd(int device_fd, int iommufd)
 	};
 
 	ioctl_assert(device_fd, VFIO_DEVICE_BIND_IOMMUFD, &args);
+	return args.out_devid;
 }
 
 static void vfio_device_attach_iommufd_pt(int device_fd, u32 pt_id)
@@ -336,8 +337,23 @@ static void vfio_pci_iommufd_setup(struct vfio_pci_device *device,
 		free((void *)cdev_path);
 	}
 
-	vfio_device_bind_iommufd(device->fd, device->iommu->iommufd);
+	device->dev_id = vfio_device_bind_iommufd(device->fd, device->iommu->iommufd);
 	vfio_device_attach_iommufd_pt(device->fd, device->iommu->ioas_id);
+}
+
+void vfio_pci_device_attach_iommu(struct vfio_pci_device *device, struct iommu *iommu)
+{
+	u32 pt_id = iommu->ioas_id;
+
+	/* Only iommufd supports changing struct iommu attachments */
+	VFIO_ASSERT_TRUE(iommu->iommufd);
+
+	if (iommu->hwpt_id)
+		pt_id = iommu->hwpt_id;
+
+	VFIO_ASSERT_NE(pt_id, 0);
+	vfio_device_attach_iommufd_pt(device->fd, pt_id);
+	device->iommu = iommu;
 }
 
 struct vfio_pci_device *vfio_pci_device_alloc(const char *bdf, struct iommu *iommu)
