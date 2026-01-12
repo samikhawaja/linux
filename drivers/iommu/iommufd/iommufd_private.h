aba01b6fa44b65561daa6928e95e0e7f92912d2b
YiFei Zhu <zhuyifei@google.com>
2026-01-12 19:08:04 +0000

    iommufd-lu: Make iopt mappings immutable after hwpt is preserved
    The map and unmap ioctls operates directly on iopt, which contains
    an array of domains, while each hwpt contains only one domain. The
    logic then becomes that mapping and nmapping is proibited if
    any of the domains in an iopt belongs to a preserved hwpt. However,
    tracing to the hwpt through the domain is a lot more tedious than
    tracing through the ioas, so if an hwpt is preserved,
    hwpt->ioas->iopt is made immutable.
    
    When undoing this (making the iopts mutable again), there's never
    a need to make some iopts mutable and some kept immutable, since
    the undo only happen on unpreserve and error path of preserve.
    Simply iterate all the ioas and clear the immutability flag on all
    their iopts.
    
    Signed-off-by: YiFei Zhu <zhuyifei@google.com>

--- drivers/iommu/iommufd/iommufd_private.h
+++ drivers/iommu/iommufd/iommufd_private.h
@@ -101,6 +101,9 @@ struct io_pagetable {
 	/* IOVA that cannot be allocated, struct iopt_reserved */
 	struct rb_root_cached reserved_itree;
 	u8 disable_large_pages;
+#ifdef CONFIG_LIVEUPDATE
+	bool lu_map_immutable;
+#endif
 	unsigned long iova_alignment;
 };
 
@@ -729,6 +732,10 @@ int iommufd_liveupdate_unregister_lufs(void);
 
 int iommufd_hwpt_lu_set_preserved(struct iommufd_ucmd *ucmd);
 int iommufd_hwpt_lu_restore(struct iommufd_ucmd *ucmd);
+static inline bool iopt_lu_map_immutable(const struct io_pagetable *iopt)
+{
+	return iopt->lu_map_immutable;
+}
 #else
 static inline int iommufd_liveupdate_register_lufs(void)
 {
@@ -748,6 +755,11 @@ static inline int iommufd_hwpt_lu_set_preserved(struct iommufd_ucmd *ucmd)
 static inline int iommufd_hwpt_lu_restore(struct iommufd_ucmd *ucmd)
 {
 	return -ENOTTY;
+}
+
+static inline bool iopt_lu_map_immutable(const struct io_pagetable *iopt)
+{
+	return false;
 }
 #endif
 
