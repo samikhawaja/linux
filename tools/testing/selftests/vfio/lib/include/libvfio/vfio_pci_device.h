51f14cb7dd70315c9fb6dddcfce6aa196a1cf986
Samiullah Khawaja <skhawaja@google.com>
2026-01-27 19:18:18 +0000

    vfio: selftests: Allow set up of pci device without attaching iommu
    Modern iommufd API allows setting up an IOAS and HWPT without attaching
    it to the device. But this can only be done after binding the device to
    iommufd. Expose the iommufd bind API as this operation can be done
    independent of iommu attach.
    
    On flip side, the device can be setup without attaching to an iommu. The
    test writer needs to make sure that the device is attached to an iommu
    if needed for a device operation.
    
    Also after liveupdate the preserved devices are attached to the
    underlying iommu during boot, so device can be setup without attaching
    to a new iommu and operate properly.
    
    Signed-off-by: Samiullah Khawaja <skhawaja@google.com>

--- tools/testing/selftests/vfio/lib/include/libvfio/vfio_pci_device.h
+++ tools/testing/selftests/vfio/lib/include/libvfio/vfio_pci_device.h
@@ -42,6 +42,10 @@ struct vfio_pci_device {
 struct vfio_pci_device *__vfio_pci_device_init(const char *bdf,
 					       struct iommu *iommu,
 					       int device_fd);
+int vfio_device_bind_iommufd(int device_fd, int iommufd);
+struct vfio_pci_device *__vfio_pci_device_noattach_init(const char *bdf,
+							int device_fd,
+							struct iommu *iommu);
 struct vfio_pci_device *vfio_pci_device_init(const char *bdf, struct iommu *iommu);
 void vfio_pci_device_cleanup(struct vfio_pci_device *device);
 
