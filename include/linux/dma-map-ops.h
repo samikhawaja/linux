40d5551f004c73d43aa99ef61184934a9485af2a
Samiullah Khawaja <skhawaja@google.com>
2026-04-28 21:52:02 +0000

    dma/pool: Add an API to check if DMA allocation is from pool
    DMA allocations can be done through DMA pools, add an API that can be
    used to check if an allocation is done from a pool. This will be used in
    the later commit during preservation of DMA allocation.
    
    Signed-off-by: Samiullah Khawaja <skhawaja@google.com>

--- include/linux/dma-map-ops.h
+++ include/linux/dma-map-ops.h
@@ -216,6 +216,7 @@ struct page *dma_alloc_from_pool(struct device *dev, size_t size,
 		bool (*phys_addr_ok)(struct device *, phys_addr_t, size_t));
 bool dma_free_from_pool(struct device *dev, void *start, size_t size);
 
+bool dma_is_from_pool(void *start, size_t size);
 int dma_direct_set_offset(struct device *dev, phys_addr_t cpu_start,
 		dma_addr_t dma_start, u64 size);
 
