56dac9f0be00a80411d0b7e2936ee5109176a7ce
Samiullah Khawaja <skhawaja@google.com>
2026-06-09 17:33:44 +0000

    iommu/vt-d: Handle reattach of the restored domain
    Reattach the restored domain to the preserved device using restored
    domain ID. While reattaching do not setup the context and PASID entries
    as those are preserved during liveupdate.
    
    Signed-off-by: Samiullah Khawaja <skhawaja@google.com>

--- drivers/iommu/intel/iommu.h
+++ drivers/iommu/intel/iommu.h
@@ -1297,6 +1297,9 @@ void intel_iommu_unpreserve(struct iommu_device *iommu,
 void clear_unpreserved_context_entries(struct intel_iommu *iommu);
 void intel_iommu_liveupdate_restore_root_table(struct intel_iommu *iommu,
 					       struct iommu_hw_ser *iommu_ser);
+int intel_iommu_domain_reattach_iommu(struct dmar_domain *domain,
+				      struct intel_iommu *iommu,
+				      struct iommu_device_ser *device_ser);
 #else
 static inline int intel_iommu_preserve_device(struct device *dev,
 					      struct iommu_device_ser *device_ser)
@@ -1322,6 +1325,12 @@ static inline void clear_unpreserved_context_entries(struct intel_iommu *iommu)
 static inline void intel_iommu_liveupdate_restore_root_table(struct intel_iommu *iommu,
 							     struct iommu_hw_ser *iommu_ser)
 {
+}
+static inline int intel_iommu_domain_reattach_iommu(struct dmar_domain *domain,
+						    struct intel_iommu *iommu,
+						    struct iommu_device_ser *device_ser)
+{
+	return -EOPNOTSUPP;
 }
 #endif
 
