24cccbce52c85b4355f9ebbb94b644d74832a474
Samiullah Khawaja <skhawaja@google.com>
2026-07-08 00:06:59 +0000

    test passes with kunit stuff

--- kernel/dma/direct_test.c
+++ kernel/dma/direct_test.c
@@ -8,6 +8,7 @@
 #include <kunit/static_stub.h>
 #include <linux/dma-map-ops.h>
 #include <linux/dma-mapping.h>
+#include <linux/dma-direct.h>
 #include <linux/kho/abi/dma_alloc.h>
 #include <linux/kexec_handover.h>
 #include <linux/slab.h>
@@ -49,30 +50,18 @@ static struct folio *mock_kho_restore_folio(phys_addr_t phys)
 	return folio;
 }
 
-static bool mock_dev_is_dma_coherent(struct device *dev)
-{
-	return false;
-}
-
 static void test_dma_direct_preserve_restore_common(struct kunit *test,
 						    bool coherent,
+						    bool atomic,
 						    size_t size)
 {
-	struct device dev = {0};
-	void *addr1, *addr2;
-	dma_addr_t handle1, handle2;
 	unsigned long nr_pages = 1 << get_order(size);
+	dma_addr_t handle1, handle2;
+	int ret, expected_ret = 0;
+	struct device dev = {0};
+	gfp_t gfp = GFP_KERNEL;
+	void *addr1, *addr2;
 	u64 state;
-	int ret;
-
-#if defined(CONFIG_ARCH_HAS_SYNC_DMA_FOR_DEVICE) || \
-    defined(CONFIG_ARCH_HAS_SYNC_DMA_FOR_CPU) || \
-    defined(CONFIG_ARCH_HAS_SYNC_DMA_FOR_CPU_ALL)
-	dev.dma_coherent = coherent;
-#else
-	if (!coherent)
-		kunit_activate_static_stub(test, dev_is_dma_coherent, mock_dev_is_dma_coherent);
-#endif
 
 	kunit_activate_static_stub(test, kho_restore_pages, mock_kho_restore_pages);
 	kunit_activate_static_stub(test, kho_restore_folio, mock_kho_restore_folio);
@@ -80,39 +69,71 @@ static void test_dma_direct_preserve_restore_common(struct kunit *test,
 	device_initialize(&dev);
 	dev.coherent_dma_mask = DMA_BIT_MASK(64);
 	dev.dma_mask = &dev.coherent_dma_mask;
+#if defined(CONFIG_ARCH_HAS_SYNC_DMA_FOR_DEVICE) || \
+    defined(CONFIG_ARCH_HAS_SYNC_DMA_FOR_CPU) || \
+    defined(CONFIG_ARCH_HAS_SYNC_DMA_FOR_CPU_ALL)
+	dev.dma_coherent = coherent;
+#endif
+
+
+	if (atomic)
+		gfp = GFP_ATOMIC;
 
 	/* use atomic so it doesn't use CMA */
-	addr1 = dma_alloc_coherent(&dev, size, &handle1, GFP_ATOMIC);
+	addr1 = dma_alloc_coherent(&dev, size, &handle1, gfp);
 	if (!addr1) {
 		kunit_skip(test, "DMA allocation failed (unsupported configuration)");
 		return;
 	}
 
+	if (dma_is_from_pool(addr1, size)) {
+		expected_ret = -EOPNOTSUPP;
+		kunit_info(test, "DMA allocation using pool, expecting -EOPNOTSUPP");
+	} else if (dma_is_from_cma(dma_to_phys(&dev, handle1), size)) {
+		expected_ret = -EOPNOTSUPP;
+		kunit_info(test, "DMA allocation using CMA, expecting -EOPNOTSUPP");
+	}
+
+
 	ret = dma_preserve_coherent_allocation(&dev, addr1, size, handle1, &state);
-	KUNIT_EXPECT_EQ(test, ret, 0);
+	KUNIT_ASSERT_EQ(test, ret, expected_ret);
 
-	KUNIT_EXPECT_TRUE_MSG(test, kho_test_pages_preserved(virt_to_phys(addr1), nr_pages), "Allocated block not tracked in KHO");
+	if (!expected_ret) {
+		KUNIT_EXPECT_TRUE_MSG(test, kho_test_pages_preserved(dma_to_phys(&dev, handle1), nr_pages), "Allocated block not tracked in KHO");
 
-	addr2 = dma_restore_coherent_allocation(&dev, size, &handle2, GFP_KERNEL, state);
-	KUNIT_ASSERT_NOT_NULL(test, addr2);
+		addr2 = dma_restore_coherent_allocation(&dev, size, &handle2, gfp, state);
+		KUNIT_ASSERT_NOT_NULL(test, addr2);
 
-	KUNIT_EXPECT_EQ(test, handle1, handle2);
-
-	dma_free_coherent(&dev, size, addr2, handle2);
-
-	KUNIT_EXPECT_FALSE_MSG(test, kho_test_pages_preserved(virt_to_phys(addr1), nr_pages), "Allocated block still tracked after free");
+		KUNIT_EXPECT_EQ(test, handle1, handle2);
+		dma_free_coherent(&dev, size, addr2, handle2);
+		KUNIT_EXPECT_FALSE_MSG(test, kho_test_pages_preserved(dma_to_phys(&dev, handle1), nr_pages), "Allocated block still tracked after free");
+	} else {
+		dma_free_coherent(&dev, size, addr1, handle1);
+	}
 }
 
 static void test_dma_direct_coherent(struct kunit *test)
 {
 	const size_t *size = test->param_value;
-	test_dma_direct_preserve_restore_common(test, true, *size);
+	test_dma_direct_preserve_restore_common(test, true, false, *size);
 }
 
 static void test_dma_direct_non_coherent(struct kunit *test)
 {
 	const size_t *size = test->param_value;
-	test_dma_direct_preserve_restore_common(test, false, *size);
+	test_dma_direct_preserve_restore_common(test, false, false, *size);
+}
+
+static void test_dma_direct_coherent_atomic(struct kunit *test)
+{
+	const size_t *size = test->param_value;
+	test_dma_direct_preserve_restore_common(test, true, true, *size);
+}
+
+static void test_dma_direct_non_coherent_atomic(struct kunit *test)
+{
+	const size_t *size = test->param_value;
+	test_dma_direct_preserve_restore_common(test, false, true, *size);
 }
 
 static void test_dma_direct_cma(struct kunit *test)
@@ -154,6 +175,8 @@ static void test_dma_direct_cma(struct kunit *test)
 static struct kunit_case dma_direct_test_cases[] = {
 	KUNIT_CASE_PARAM(test_dma_direct_coherent, dma_size_gen_params),
 	KUNIT_CASE_PARAM(test_dma_direct_non_coherent, dma_size_gen_params),
+	KUNIT_CASE_PARAM(test_dma_direct_coherent_atomic, dma_size_gen_params),
+	KUNIT_CASE_PARAM(test_dma_direct_non_coherent_atomic, dma_size_gen_params),
 	KUNIT_CASE(test_dma_direct_cma),
 	{}
 };
