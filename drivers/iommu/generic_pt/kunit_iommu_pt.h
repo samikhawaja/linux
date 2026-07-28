737340007b1e146242246a413012edc52873e5de
Samiullah Khawaja <skhawaja@google.com>
2026-07-28 00:59:19 +0000

    more fixes

--- drivers/iommu/generic_pt/kunit_iommu_pt.h
+++ drivers/iommu/generic_pt/kunit_iommu_pt.h
@@ -443,15 +443,13 @@ static void test_restore_free(struct kunit *test)
 	do_map(test, start, start, len);
 
 	/*
-	 * Simulate a restored state by clearing all features except
-	 * SIGN_EXTEND. This verifies that the generic page table free walker
-	 * can correctly tear down a populated domain when other features are
-	 * zeroed. Also set max_vasz_lg2 as done by the actual restore()
+	 * Simulate a restored state by clearing all features except SIGN_EXTEND
+	 * and DMA_INCOHERENT. This verifies that the generic page table free
+	 * walker can correctly tear down a populated domain when other features
+	 * are zeroed. Also set max_vasz_lg2 as done by the actual restore()
 	 * function.
 	 */
-	top_range = pt_top_range(priv->common);
-	priv->common->max_vasz_lg2 = top_range.max_vasz_lg2;
-	priv->common->features &= BIT(PT_FEAT_SIGN_EXTEND);
+	priv->common->features &= (BIT(PT_FEAT_SIGN_EXTEND) | BIT(PT_FEAT_DMA_INCOHERENT));
 	priv->common->max_vasz_lg2 = top_range.max_vasz_lg2;
 
 	/* The domain will be freed when the test exits. */
