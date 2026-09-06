8614165a391a3c74a144b072073777facac33d78
Samiullah Khawaja <skhawaja@google.com>
2026-09-06 22:39:18 +0000

    iommu: Add API to reclaim the dma ownership of restored device
    Signed-off-by: Samiullah Khawaja <skhawaja@google.com>

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -1262,6 +1262,8 @@ bool iommu_group_dma_owner_claimed(struct iommu_group *group);
 bool iommu_group_is_singleton(struct iommu_group *group);
 
 int iommu_device_claim_dma_owner(struct device *dev, void *owner);
+int iommu_device_reclaim_dma_owner(struct device *dev, void *owner,
+                                   u64 restore_token);
 void iommu_device_release_dma_owner(struct device *dev);
 
 int iommu_attach_device_pasid(struct iommu_domain *domain,
